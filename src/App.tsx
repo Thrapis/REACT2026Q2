c0f2c0597eed742227567c9f9575f76d4c48c714
Thrapis <artbelski@gmail.com>
2026-05-04 23:44:28 +0300

    feat: save last search term with Local Storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
+const LAST_SEARCH_KEY = 'last_search';
+
+interface AppState {
+  initSearch: string | null;
+}
+
+class App extends React.Component<{}, AppState> {
+  searchInput: React.RefObject<HTMLInputElement | null>;
+
+  constructor(props: {}) {
+    super(props);
+    this.state = { initSearch: null };
+
+    const lastSearched = localStorage.getItem(LAST_SEARCH_KEY);
+    if (lastSearched !== null) {
+      this.state = { initSearch: lastSearched };
+    }
+    this.searchInput = React.createRef();
+  }
+
+  componentDidMount(): void {
+    if (
+      this.searchInput &&
+      this.searchInput.current &&
+      this.state.initSearch !== null
+    ) {
+      this.searchInput.current.value = this.state.initSearch;
+    }
+  }
+
+  handleSearch = () => {
+    const query = this.searchInput?.current?.value;
+    if (query !== undefined) {
+      localStorage.setItem(LAST_SEARCH_KEY, query);
+    }
+  };
+
   render() {
     return (
       <>
         <section className="top-controls-section">
-          <input type="text" />
-          <button>Search</button>
+          <input type="text" ref={this.searchInput} />
+          <button onClick={this.handleSearch}>Search</button>
         </section>
 
         <section className="results-section">
