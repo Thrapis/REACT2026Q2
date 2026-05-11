73684ceb28df3d7e4a7777f743bd5e345fca5a9f
Thrapis <artbelski@gmail.com>
2026-05-11 20:21:23 +0300

    test: add unit tests for api functions

---
+++ src/api/RickAndMortyAPI.test.ts
@@ -0,0 +1,54 @@
+import { it, expect, vi, describe, beforeEach } from 'vitest';
+import { searchCharacters, getCharactersPage } from './RickAndMortyAPI';
+
+describe('API functions', () => {
+  beforeEach(() => {
+    vi.stubGlobal('fetch', vi.fn());
+  });
+
+  it('searchCharacters should return data', async () => {
+    const mockData = { info: {}, results: [{ id: 1, name: 'Rick' }] };
+
+    vi.mocked(fetch).mockResolvedValue({
+      ok: true,
+      json: async () => mockData,
+    } as Response);
+
+    const result = await searchCharacters('Rick', 1);
+
+    expect(fetch).toHaveBeenCalledWith(
+      'https://rickandmortyapi.com/api/character?name=Rick&page=1'
+    );
+    expect(result).toEqual(mockData);
+  });
+
+  it('searchCharacters should return error if response.ok === false', async () => {
+    const errorText = 'There is nothing here';
+    const errorResponse = { error: errorText };
+
+    vi.mocked(fetch).mockResolvedValue({
+      ok: false,
+      json: async () => errorResponse,
+    } as Response);
+
+    await expect(searchCharacters('SomeUnrealShite')).rejects.toThrow(
+      errorText
+    );
+  });
+
+  it('getCharactersPage should make request using URL', async () => {
+    const mockData = { info: {}, results: [] };
+    const testUrl =
+      'https://rickandmortyapi.com/api/character?page=2&name=rick';
+
+    vi.mocked(fetch).mockResolvedValue({
+      ok: true,
+      json: async () => mockData,
+    } as Response);
+
+    const result = await getCharactersPage(testUrl);
+
+    expect(fetch).toHaveBeenCalledWith(testUrl);
+    expect(result).toEqual(mockData);
+  });
+});
