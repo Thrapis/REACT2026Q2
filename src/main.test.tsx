9b540d297ab97a9f2e5547885751bc5ad7f477c2
Thrapis <artbelski@gmail.com>
2026-05-11 20:08:55 +0300

    init: start unit-testing task and configure vitest + msw for unit testing

---
+++ src/main.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Example', () => {
+  it('checks if one is one', () => {
+    expect(1).toBe(1);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src/'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
+});
